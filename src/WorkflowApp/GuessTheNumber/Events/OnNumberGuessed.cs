2a9e7c6a98a91fe79f5d35cb0f51ecbf6bf098bd
Petrutiu Mihai <mihai.petrutiu@qubiz.com>
2017-05-22 17:59:48 +0300

    Add description to event

--- src/WorkflowApp/GuessTheNumber/Events/OnNumberGuessed.cs
+++ src/WorkflowApp/GuessTheNumber/Events/OnNumberGuessed.cs
@@ -1,7 +1,9 @@
-﻿using EventProcessing.Core.EventStore;
+﻿using System.ComponentModel;
+using EventProcessing.Core.EventStore;
 
 namespace WorkflowApp.GuessTheNumber.Events
 {
+    [Description("Raised when the generated number is guessed")]
     public class OnNumberGuessed : FlowEvent
     {
         private int randomValue;
